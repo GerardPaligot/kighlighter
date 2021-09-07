8753e2ce5f98ec94d9152da44ef42897432b3c0f
GerardPaligot <gerard.paligot@gmail.com>
2021-09-07 21:05:28 +0200

    refactor: Clean undesired logs during desktop execution.

--- core/src/desktopMain/kotlin/com/paligot/kighlighter/core/LanguagePatternDesktop.kt
+++ core/src/desktopMain/kotlin/com/paligot/kighlighter/core/LanguagePatternDesktop.kt
@@ -3,8 +3,4 @@ package com.paligot.kighlighter.core
 actual fun <T : ColorScheme> LanguagePattern<T>.findAllSnippetRanges(snippet: String, colorScheme: T): Sequence<SnippetRange> =
     pattern.findAll(snippet)
         .filter { it.groups.isNotEmpty() && it.groups[1] != null }
-        .map {
-        println(pattern.pattern)
-        println(it.groups)
-        SnippetRange(it.groups[1]!!.range, this.color(colorScheme))
-    }
+        .map { SnippetRange(it.groups[1]!!.range, this.color(colorScheme)) }
