55d70b60e211c6084fe1527027cd554215524aa5
GerardPaligot <gerard.paligot@gmail.com>
2021-09-17 00:01:45 +0200

    fix: Avoid error when there is no group in lang regex for android.

--- core/src/androidMain/kotlin/com/paligot/kighlighter/core/LanguagePatternAndroid.kt
+++ core/src/androidMain/kotlin/com/paligot/kighlighter/core/LanguagePatternAndroid.kt
@@ -1,4 +1,6 @@
 package com.paligot.kighlighter.core
 
 actual fun <T : ColorScheme> LanguagePattern<T>.findAllSnippetRanges(snippet: String, colorScheme: T): Sequence<SnippetRange> =
-    pattern.findAll(snippet).map { SnippetRange(it.groups[1]!!.range, this.color(colorScheme)) }
+    pattern.findAll(snippet)
+        .filter { it.groups.isNotEmpty() && it.groups[1] != null }
+        .map { SnippetRange(it.groups[1]!!.range, this.color(colorScheme)) }
