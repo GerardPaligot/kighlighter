a04295ee5be9367f72994b58aa0b62bdc909b85e
GerardPaligot <gerard.paligot@gmail.com>
2021-09-17 21:53:38 +0200

    refactor: Use new Window API for desktop sample

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,21 +1,23 @@
-import androidx.compose.desktop.Window
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 import com.paligot.kighlighter.components.kotlin.Kotlin
-import com.paligot.kighlighter.components.xml.Xml
 import com.paligot.kighlighter.compose.Kighlighter
 import com.paligot.kighlighter.palettes.Palettes
 
-fun main() = Window {
-    Kighlighter(
-        snippet = kotlinSample,
-        language = Kotlin(palette = Palettes.Prism),
-        linesHighlighted = arrayListOf(3, 6, 7),
-        modifier = Modifier.height(300.dp)
-    )
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        Kighlighter(
+            snippet = kotlinSample,
+            language = Kotlin(palette = Palettes.Prism),
+            linesHighlighted = arrayListOf(3, 6, 7),
+            modifier = Modifier.height(300.dp)
+        )
+    }
 }
 
 
