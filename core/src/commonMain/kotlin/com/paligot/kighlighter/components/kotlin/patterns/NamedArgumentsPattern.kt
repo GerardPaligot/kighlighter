4001f7f7bbb9996ecfc3d9d34dde6d2523c143df
GerardPaligot <gerard.paligot@gmail.com>
2021-09-19 00:08:33 +0200

    fix: Apply color for all named args for kotlin code.

--- core/src/commonMain/kotlin/com/paligot/kighlighter/components/kotlin/patterns/NamedArgumentsPattern.kt
+++ core/src/commonMain/kotlin/com/paligot/kighlighter/components/kotlin/patterns/NamedArgumentsPattern.kt
@@ -6,7 +6,7 @@ import com.paligot.kighlighter.core.LanguagePattern
 
 object NamedArgumentsPattern: LanguagePattern<KotlinColorScheme> {
     override val pattern: Regex
-        get() = "\\([ \t\n]*(\\w+[ \t\n]*=(?![=!&<>/]))[ \t\n]*".toRegex()
+        get() = "(?:\\(|,)[ \t\n]*(\\w+[ \t\n]*=(?![=!&<>/]))[ \t\n]*".toRegex()
 
     override fun color(colorScheme: KotlinColorScheme): Color = colorScheme.namedArguments
 }
