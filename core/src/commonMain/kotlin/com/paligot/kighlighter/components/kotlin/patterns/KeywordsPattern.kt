b7b168a47c624c4ef1d17f33b98182f8b7d3dea2
GerardPaligot <gerard.paligot@gmail.com>
2021-09-18 21:43:27 +0200

    fix: Keywords should start by a space or tab.

--- core/src/commonMain/kotlin/com/paligot/kighlighter/components/kotlin/patterns/KeywordsPattern.kt
+++ core/src/commonMain/kotlin/com/paligot/kighlighter/components/kotlin/patterns/KeywordsPattern.kt
@@ -6,7 +6,7 @@ import com.paligot.kighlighter.core.LanguagePattern
 
 internal object KeywordsPattern: LanguagePattern<KotlinColorScheme> {
     override val pattern: Regex
-        get() = "(?:^|[^.])(?:\\b)?(abstract|actual|annotation|as|break|by|catch|class|companion|const|constructor|continue|crossinline|data|do|dynamic|else|enum|expect|external|final|finally|for|fun|get|if|import|in|infix|init|inline|inner|interface|internal|is|lateinit|noinline|null|object|open|operator|out|override|package|private|protected|public|reified|return|sealed|set|super|suspend|tailrec|this|throw|to|try|typealias|val|var|vararg|when|where|while)(?:\\b)".toRegex()
+        get() = "(?:^|[^.])(?:\\b)(abstract|actual|annotation|as|break|by|catch|class|companion|const|constructor|continue|crossinline|data|do|dynamic|else|enum|expect|external|final|finally|for|fun|get|if|import|in|infix|init|inline|inner|interface|internal|is|lateinit|noinline|null|object|open|operator|out|override|package|private|protected|public|reified|return|sealed|set|super|suspend|tailrec|this|throw|to|try|typealias|val|var|vararg|when|where|while)(?:\\b)".toRegex()
 
     override fun color(colorScheme: KotlinColorScheme): Color = colorScheme.keywords
 }
